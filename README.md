3d96d1f318cd0c28a81fb4a28159d7f8a9e23c75
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-04-20 18:47:41 +0300

    final work for lab02

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # lab02_JQuery
 
 Number and name of feature: 1
-<<<<<<< HEAD
 Estimate of time needed to complete: 20min
 Start time: 3:40
 Finish time: 4:20
@@ -18,19 +17,3 @@ Estimate of time needed to complete: 20min
 Start time: 4:40
 Finish time: 5:00
 Actual time needed to complete: 20min
-=======
-Estimate of time needed to complete: 20
-Start time: 3:40
-Finish time: **\_**
-Actual time needed to complete: **\_**
-Number and name of feature: 1
-Estimate of time needed to complete: 20
-Start time: 3:40
-Finish time: **\_**
-Actual time needed to complete: **\_**
-Number and name of feature: 1
-Estimate of time needed to complete: 20
-Start time: 3:40
-Finish time: **\_**
-Actual time needed to complete: **\_**
->>>>>>> 8244417cd1d1c8d9fc83d0a8716223fecd84c977
--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 "use strict";
 /*-----read from json file---- */
-<<<<<<< HEAD
 let keyWords = [];
 
 function Image(item) {
@@ -25,39 +24,12 @@ Image.prototype.render = function () {
 };
 
 function readJson() {
-=======
-let My_array = [];
-let toPush = [];
-function Image(url, title, description, keyword, horns) {
-  this.title = title;
-  this.img = url;
-  this.description = description;
-  this.keyword = keyword;
-  this.horns = horns;
-  toPush.push(this);
-}
-console.log(toPush);
-
-Image.prototype.render = function () {
-  let $photo = $(
-    `<div id="photo-template" class=${this.keyword}></div>`
-  ).clone(); ///array
-
-  $photo.append($(`<h2>${this.title}</h2>`));
-  $photo.append($(`<img src=${this.img}></img>`));
-  $photo.append($(`<p>${this.description}</p>`));
-  $("main").append($photo);
-};
-
-Image.readJson = () => {
->>>>>>> 8244417cd1d1c8d9fc83d0a8716223fecd84c977
   const ajaxSetting = {
     method: "get",
     datatype: "json",
   };
 
   $.ajax("data/page-1.json", ajaxSetting).then(doStuff);
-<<<<<<< HEAD
 }
 
 readJson();
@@ -77,33 +49,10 @@ function renderKeywords() {
     let $keyword = $(".option").clone();
     $keyword.text(item);
     $("select").append($keyword);
-=======
-};
-
-function doStuff(data) {
-  data.forEach((item, i) => {
-    let { image_url, title, description, keyword, horns } = item;
-    new Image(image_url, title, description, keyword, horns).render();
-    if (!My_array.includes(keyword)) My_array.push(keyword);
-  });
-  filterr();
-}
-$(() => Image.readJson());
-/*--------------filter----------------*/
-function filterr() {
-  $("select").on("change", filterFunction);
-
-  My_array.forEach((item, i) => {
-    let $keyword = $(".option").clone();
-    $keyword.text(item);
-    $("select").append($keyword);
-    $keyword.attr("id", i);
->>>>>>> 8244417cd1d1c8d9fc83d0a8716223fecd84c977
     $keyword.attr("value", item);
     $keyword.removeClass("option");
   });
 }
-<<<<<<< HEAD
 
 $("select").on("change", filterFunction);
 function filterFunction() {
@@ -114,10 +63,4 @@ function filterFunction() {
     $("div").hide();
     $(`.${select}`).show();
   }
-=======
-function filterFunction() {
-  let select = $(this).children("option:selected").val();
-  $("main").children().addClass("hide");
-  $(`.${select}`).removeClass("hide");
->>>>>>> 8244417cd1d1c8d9fc83d0a8716223fecd84c977
 }
