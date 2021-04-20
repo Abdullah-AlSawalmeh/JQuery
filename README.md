2235ac5a23fa3b847eeea453df1a32bd7fc02751
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-04-20 18:44:40 +0300

    merging

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # lab02_JQuery
+
+Number and name of feature: 1
+Estimate of time needed to complete: 20min
+Start time: 3:40
+Finish time: 4:20
+Actual time needed to complete: 20min
+
+Number and name of feature: 2
+Estimate of time needed to complete: 20min
+Start time: 4:20
+Finish time: 4:40
+Actual time needed to complete: 20min
+
+Number and name of feature: 3
+Estimate of time needed to complete: 20min
+Start time: 4:40
+Finish time: 5:00
+Actual time needed to complete: 20min
---
+++ js/app.js
@@ -0,0 +1,66 @@
+"use strict";
+/*-----read from json file---- */
+let keyWords = [];
+
+function Image(item) {
+  this.title = item.title;
+  this.image_url = item.image_url;
+  this.description = item.description;
+  this.keyword = item.keyword;
+  this.horns = item.horns;
+}
+
+Image.prototype.render = function () {
+  let hornClone = $(".photo-template").clone();
+  hornClone.find("h2").text(this.title);
+  hornClone.find("p").text(this.description);
+  hornClone.find("img").attr({
+    src: this.image_url,
+    alt: this.title,
+  });
+  hornClone.attr("class", `${this.keyword}`);
+  hornClone.attr("id", "photo-template");
+  $("main").append(hornClone);
+};
+
+function readJson() {
+  const ajaxSetting = {
+    method: "get",
+    datatype: "json",
+  };
+
+  $.ajax("data/page-1.json", ajaxSetting).then(doStuff);
+}
+
+readJson();
+
+function doStuff(HornData) {
+  HornData.forEach((item) => {
+    let newImage = new Image(item);
+    newImage.render();
+    if (!keyWords.includes(newImage.keyword)) keyWords.push(newImage.keyword);
+  });
+  $(".photo-template").first().remove();
+  renderKeywords();
+}
+/*--------------filter----------------*/
+function renderKeywords() {
+  keyWords.forEach((item) => {
+    let $keyword = $(".option").clone();
+    $keyword.text(item);
+    $("select").append($keyword);
+    $keyword.attr("value", item);
+    $keyword.removeClass("option");
+  });
+}
+
+$("select").on("change", filterFunction);
+function filterFunction() {
+  let select = $(this).val();
+  if (select == "default") {
+    $("div").show();
+  } else {
+    $("div").hide();
+    $(`.${select}`).show();
+  }
+}
