41fc20dac35a50cf973643767ff0d2de9f2a862c
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-04-20 18:07:02 +0300

    New changes

--- js/app.js
+++ js/app.js
@@ -1,58 +1,101 @@
-'use strict';
+"use strict";
 /*-----read from json file---- */
 let My_array = [];
-function Image(url, title, description, keyword, horns) {
-  this.title = title;
-  this.img = url;
-  this.description = description;
-  this.keyword = keyword;
-  this.horns = horns;
+let toPush = [];
+
+function Image(item) {
+  this.title = item.title;
+  this.img = item.image_url;
+  this.description = item.description;
+  this.keyword = item.keyword;
+  this.horns = item.horns;
+  toPush.push(this);
 }
+console.log('aaaaaaaaaaaaaaaaa',toPush[0]);
 
 Image.prototype.render = function () {
-  let $photo = $(
-    `<div id="photo-template" class=${this.keyword}></div>`
-  ).clone(); ///array
+  // let $photo = $(
+    //   `<div id="photo-template" class=${this.keyword}></div>`
+    // ).clone(); ///array
+    
+    // $photo.append($(`<h2>${this.title}</h2>`));
+  // $photo.append($(`<img src=${this.img}></img>`));
+  // $photo.append($(`<p>${this.description}</p>`));
+  // $("main").append($photo);
 
-  $photo.append($(`<h2>${this.title}</h2>`));
-  $photo.append($(`<img src=${this.img}></img>`));
-  $photo.append($(`<p>${this.description}</p>`));
-  $('main').append($photo);
+  let hornClone = $('.photo-template').clone();
+  hornClone.find('h2').text(this.title);
+  hornClone.find('p').text(this.description);
+  hornClone.find('img').attr({
+    src:this.img,
+    alt:this.title
+  })
+  hornClone.attr('class',this.keyword);
+  $("main").append(hornClone);
 };
 
-Image.readJson = () => {
+function readJson (){
   const ajaxSetting = {
-    method: 'get',
-    datatype: 'json',
+    method: "get",
+    datatype: "json",
   };
 
-  $.ajax('data/page-1.json', ajaxSetting).then(doStuff);
-};
-
-function doStuff(data) {
-  data.forEach((item, i) => {
-    let { image_url, title, description, keyword, horns } = item;
-    new Image(image_url, title, description, keyword, horns).render();
-    if (!My_array.includes(keyword)) My_array.push(keyword);
-  });
-  filterr();
+  $.ajax("data/page-1.json", ajaxSetting).then(doStuff);
 }
-$(() => Image.readJson());
+
+// $(() =>readJson());
+readJson();
+
+function doStuff(HornData) {
+  // console.log({HornData});
+  HornData.forEach((item, i) => {
+    // let { image_url, title, description, keyword, horns } = item;
+    // new Image(image_url, title, description, keyword, horns).render();
+    let newImage = new Image(item);
+    newImage.render();
+    if (!My_array.includes(newImage.keyword)) {
+      console.log('jjjjjj')
+      My_array.push(newImage.keyword);
+  }
+
+  });
+  $('.photo-template').first().remove();
+console.log('aaaaaaaaaaaaaaaaa',toPush[0]);
+
+  // $('#photo-template').remove();
+  renderKeywords();
+}
 /*--------------filter----------------*/
-function filterr() {
-  $('select').on('change', filterFunction);
-
+function renderKeywords() {
   My_array.forEach((item, i) => {
-    let $keyword = $('.option').clone();
+    let $keyword = $(".option").clone();
     $keyword.text(item);
-    $('select').append($keyword);
-    $keyword.attr('id', i);
-    $keyword.attr('value', item);
-    $keyword.removeClass('option');
+    $("select").append($keyword);
+    // $keyword.attr("id", i);
+    $keyword.attr("value", item);
+    $keyword.removeClass("option");
+
+    // let optionTag = `<option value='${item}'> ${item}</option>`
+    // $("select").append(optionTag);
+
   });
 }
+
+
+$("select").on("change", filterFunction);
 function filterFunction() {
-  let select = $(this).children('option:selected').val();
-  $('main').children().addClass('hide');
-  $(`.${select}`).removeClass('hide');
+  // console.log($(this).val());
+  // let select = $(this).children("option:selected").val();
+  
+  let select = $(this).val();
+  if(select=='default') {
+    $('div').show();
+  } else {
+    $('div').hide();
+    $(`.${select}`).show();
+  }
+  // $("main").children().addClass("hide");
+  // $(`.${select}`).removeClass("hide");
+  
+
 }
